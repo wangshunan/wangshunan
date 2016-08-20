bc3628c5daaa9c7a9eb3704dde6590757359db78
wangshunan12 <wangshunan12@gmail.com>
2016-08-20 19:55:17 +0800

    オリジナルモーションをテストした。

--- Assets/script/EnemyController.cs
+++ Assets/script/EnemyController.cs
@@ -5,14 +5,52 @@ public class EnemyController : MonoBehaviour {
 
 	[SerializeField]
 	PlayerController playerController;
+	[SerializeField]
+	Rigidbody2D EnemyRd;
 
-	// Use this for initialization
+    Animator animator;
+	private SpriteRenderer spriteRenderer;
+
+	public GameObject target;
+
+	private Vector2 EnemyMove = Vector2.one;
+
+	float TargetX;
+	float EnemyX;
+	float DistanceCheck;
+	float EnemySpeed = 1f;
+	const float DISTANCE = 10.0f;
+
+
+
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+		target = GameObject.Find ("Unitychan");
+		EnemyRd = GetComponent<Rigidbody2D> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		EnemyMove = transform.position;
+		TargetX = target.transform.position.x;
+		EnemyX = transform.position.x;
+		DistanceCheck = TargetX - EnemyX;
+
+		if (TargetX > EnemyX) {
+			spriteRenderer.flipX = false;
+		} else {
+			spriteRenderer.flipX = true;
+
+		}
+
+		if (DistanceCheck > DISTANCE || DistanceCheck < -DISTANCE) {
+			EnemyRd.MovePosition (Vector2.Lerp (EnemyMove, target.transform.position, Time.deltaTime));
+			animator.SetBool ("Run", true);
+		} else {
+			animator.SetBool( "Run", false );
+			animator.SetTrigger ("HolgerAttack");
+		}
+
 	}
 }
+
