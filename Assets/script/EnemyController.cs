4f73d35aae2768eff493aae2eec79f0c849f61a1
wangshunan12 <wangshunan12@gmail.com>
2016-08-24 09:37:50 +0800

    no message

--- Assets/script/EnemyController.cs
+++ Assets/script/EnemyController.cs
@@ -5,51 +5,90 @@ public class EnemyController : MonoBehaviour {
 
 	[SerializeField]
 	PlayerController playerController;
-	[SerializeField]
+
 	Rigidbody2D EnemyRd;
-
     Animator animator;
-	private SpriteRenderer spriteRenderer;
-
+        
 	public GameObject target;
 
-	private Vector2 EnemyMove = Vector2.one;
+    // base layerで使われる、アニメーターの現在の状態の参照
+    private AnimatorStateInfo currentBaseState;
+    private SpriteRenderer spriteRenderer;
 
 	float TargetX;
 	float EnemyX;
-	float DistanceCheck;
-	float EnemySpeed = 1f;
-	const float DISTANCE = 10.0f;
+	float distanceCheck;
+	float moveSpeed = 1.0f;
+	const float HIT_DISTANCE = 1.0f;
+    const float FIND_DISTANCE = 5.0f;
+    float hitCount = 10.0f;
+
+    static int attack = Animator.StringToHash ( "Base Layer.Attack" );
+    static int damage = Animator.StringToHash ( "Base Layer.Damage" );
 
 
-
-	void Start () {
+	void Start() {
 		animator = GetComponent<Animator> ();
-		target = GameObject.Find ("Unitychan");
+		target = GameObject.Find ("Cguy");
 		EnemyRd = GetComponent<Rigidbody2D> ();
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 
-	void Update () {
-		EnemyMove = transform.position;
+	void Update() {
+
+        currentBaseState = animator.GetCurrentAnimatorStateInfo (0);
 		TargetX = target.transform.position.x;
 		EnemyX = transform.position.x;
-		DistanceCheck = TargetX - EnemyX;
+		distanceCheck = EnemyX - TargetX;
 
-		if (TargetX > EnemyX) {
+        if ( currentBaseState.fullPathHash != damage ) {
+            if ( distanceCheck > 0.0f ) {
+                spriteRenderer.flipX = true ;
+            } else { 
+                spriteRenderer.flipX = false ;
+            }
+
+            if ( ( distanceCheck > 0 ? distanceCheck : -distanceCheck ) > HIT_DISTANCE && 
+                 ( distanceCheck > 0 ? distanceCheck : -distanceCheck ) <= FIND_DISTANCE && 
+                   currentBaseState.fullPathHash != attack ) {
+
+                EnemyRd.transform.position += new Vector3( ( spriteRenderer.flipX == true ?  -moveSpeed : moveSpeed ) * Time.deltaTime, 0 );
+                animator.SetBool( "Run", true );
+
+            } else if ( ( distanceCheck > 0 ? distanceCheck : -distanceCheck ) <= HIT_DISTANCE ) {
+                animator.SetTrigger( "Attack" );
+            } else {
+                animator.SetBool( "Run", false );
+            }
+        }
+
+        if ( Input.GetKeyDown( "x" ) ) {
+            animator.SetTrigger( "Damage" );
+            EnemyRd.velocity = new Vector2( 3.0f , 2.0f );
+        }
+
+
+
+
+
+
+
+
+
+		/*if (TargetX > EnemyX) {
 			spriteRenderer.flipX = false;
 		} else {
 			spriteRenderer.flipX = true;
 
-		}
+		}*/
 
-		if (DistanceCheck > DISTANCE || DistanceCheck < -DISTANCE) {
+		/*if (DistanceCheck > DISTANCE || DistanceCheck < -DISTANCE) {
 			EnemyRd.MovePosition (Vector2.Lerp (EnemyMove, target.transform.position, Time.deltaTime));
 			animator.SetBool ("Run", true);
 		} else {
 			animator.SetBool( "Run", false );
-			animator.SetTrigger ("HolgerAttack");
-		}
+			animator.SetTrigger ("Attack");
+		}*/
 
 	}
 }
--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -5,91 +5,80 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     Animator animator;
-    CharacterController controller;
+    Rigidbody2D rig2d;
+    LayerMask groundMask;
 
     public float speed;
     public float jumpPower;
     public float gravity;
-    public GameObject target;
+    public int hp = 4;
 
 	private SpriteRenderer spriteRenderer;
 	private float distanceCheck = 0.0f;
 	private const float hitDiretion = 1.0f;
-    private Vector3 moveDiretion = Vector3.zero;
     // base layerで使われる、アニメーターの現在の状態の参照
     private AnimatorStateInfo currentBaseState;
-
+    private float axis = 0.0f;
+    
+    [SerializeField] private float characterHeightOffset = 0.2f;
 
     // アニメーター各ステートへの参照
     static int attack = Animator.StringToHash ( "Base Layer.Attack" );
     static int run = Animator.StringToHash ( "Base Layer.Run" );
-    static int jumpLanding = Animator.StringToHash( "Base Layer.Jump_Fall" );
+    static int jump = Animator.StringToHash( "Base Layer.Jump" );
 
 
 	// Use this for initialization
 	void Start () {
-
-        controller = GetComponent<CharacterController>();
         spriteRenderer = GetComponent<SpriteRenderer> ();
         animator = GetComponent<Animator>();
-
+        rig2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float axis = Input.GetAxisRaw ("Horizontal");
-        float velY = moveDiretion.y;
+        axis = Input.GetAxisRaw ("Horizontal");
         currentBaseState = animator.GetCurrentAnimatorStateInfo (0);
 
-        // 接地状態でしかできない動き
-        if( controller.isGrounded ) {
-			
-			//　キャラを移動させる
-			if( currentBaseState.fullPathHash != attack ) {
-				if( axis != 0.0f ) {
-					moveDiretion.x = axis * speed;
-					animator.SetBool( "Run", true );
-				} else {
-					moveDiretion.x = 0;
-					animator.SetBool( "Run", false );
-				}
-			}
+		    //　キャラを移動させる
+			if( axis != 0 && currentBaseState.fullPathHash != attack ) {
+                rig2d.transform.position += new Vector3( axis * speed * Time.deltaTime, 0 );  
+                animator.SetBool( "Run", true );
+			} else {
+                animator.SetBool( "Run", false );
+            }
 				
-			if( Input.GetKeyDown(KeyCode.Z)  ) {
-                distanceCheck = transform.position.x - target.gameObject.transform.position.x;
+			if( Input.GetKeyDown( KeyCode.Z ) && rig2d.velocity.y == 0 ) {
+                //distanceCheck = transform.position.x - target.gameObject.transform.position.x;
                 animator.SetTrigger( "Attack" );
-				if( ( distanceCheck < 0 && spriteRenderer.flipX == false ) ||
+                //animator.SetBool( "Run", false );
+				/*if( ( distanceCheck < 0 && spriteRenderer.flipX == false ) ||
                     ( distanceCheck > 0 && spriteRenderer.flipX == true ) ) {
 
 					if ((distanceCheck > 0 ? distanceCheck : -distanceCheck) < hitDiretion) {
-						if (target.gameObject.CompareTag ("Enemy")) {
+						/*if (target.gameObject.CompareTag ("Enemy")) {
 							Debug.Log ("Hit!!!");
 						}
 					}
-                }
+                }*/
             }
-		    if ( Input.GetButtonDown ("Jump")  ) {
-			    moveDiretion.y = jumpPower;
-                animator.SetTrigger( "Jump");      
+	        if ( Input.GetButtonDown ("Jump") ) {
+		        rig2d.velocity = new Vector2 ( rig2d.velocity.x, jumpPower );
+                animator.SetBool( "Jump", true );
 	        }
-            
-        }
-        animator.SetFloat("velY",velY);
-		moveDiretion.y -= gravity* Time.deltaTime;
-        Vector3 globaDirection = transform.TransformDirection( moveDiretion );
-        controller.Move( globaDirection * Time.deltaTime );
+
+            if ( rig2d.velocity.y == 0 ) {
+                animator.SetBool( "Jump", false );
+            }
+
+        var distanceFromGround = Physics2D.Raycast ( transform.position, Vector3.down, 1, groundMask ); 
 
 
-		if( controller.isGrounded ) {
-			moveDiretion.y = 0;
-		}
-
-		if (axis != 0 && currentBaseState.fullPathHash != attack ) {
+		if ( axis != 0 && currentBaseState.fullPathHash != attack ) {
 			spriteRenderer.flipX = axis < 0;
         }
+        Debug.Log( "Y = " + rig2d.velocity.y );
 
-	}
-
-
+    }
 }
