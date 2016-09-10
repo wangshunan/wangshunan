51938bd852bac86b0940be08722b3053cc130852
wangshunan <wangshunan12@gmail.com>
2016-09-10 11:03:11 +0900

    voiceの調整

--- Assets/script/VoiceManager.cs
+++ Assets/script/VoiceManager.cs
@@ -43,45 +43,59 @@ public class VoiceManager : MonoBehaviour {
 				Vod0010.PlayOneShot (Vod0010.clip);
 			}
 			if (count == 2) {
+				Vod0010.Stop ();
 				Guyo0020.PlayOneShot (Guyo0020.clip);
 			}
 			if (count == 3) {
+				Guyo0020.Stop ();
 				Vod0030.PlayOneShot (Vod0030.clip);
 			}
 			if (count == 4) {
+				Vod0030.Stop ();
 				Guyo0040.PlayOneShot (Guyo0040.clip);
 			}
 			if (count == 5) {
+				Guyo0040.Stop ();
 				Guyo0050.PlayOneShot (Guyo0050.clip);
 			}
 			if (count == 6) {
+				Guyo0050.Stop ();
 				Vod0060.PlayOneShot (Vod0060.clip);
 			}
 			if (count == 7) {
+				Vod0060.Stop ();
 				Guyo0070.PlayOneShot (Guyo0070.clip);
 			}
 			if (count == 8) {
+				Guyo0070.Stop ();
 				Guyo0080.PlayOneShot (Guyo0080.clip);
 			}
 			if (count == 9) {
+				Guyo0080.Stop ();
 				Vod0090.PlayOneShot (Vod0090.clip);
 			}
 			if (count == 10) {
+				Vod0090.Stop ();
 				Vod0100.PlayOneShot (Vod0100.clip);
 			}
 			if (count == 11) {
+				Vod0100.Stop ();
 				Guyo0110.PlayOneShot (Guyo0110.clip);
 			}
 			if (count == 12) {
+				Guyo0110.Stop ();
 				Vod0120.PlayOneShot (Vod0120.clip);
 			}
 			if (count == 13) {
+				Vod0120.Stop ();
 				Guyo0130.PlayOneShot (Guyo0130.clip);
 			}
 			if (count == 14) {
+				Guyo0130.Stop ();
 				Guyo0140.PlayOneShot (Guyo0140.clip);
 			}
 			if (count == 15) {
+				Guyo0140.Stop ();
 				Guyo0150.PlayOneShot (Guyo0150.clip);
 			}
 		}
